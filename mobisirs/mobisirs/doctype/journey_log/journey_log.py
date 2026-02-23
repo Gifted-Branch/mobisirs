10386128e770c26f64b52277f2d915e440bc96e0
Livuza <livuzacharles@gmail.com>
2026-02-23 17:18:13 +0300

    feat:Journey Log Doctype

---
+++ mobisirs/mobisirs/doctype/journey_log/journey_log.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Douglas Nkubitu and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class JourneyLog(Document):
+	pass
---
+++ mobisirs/mobisirs/doctype/journey_log/test_journey_log.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Douglas Nkubitu and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestJourneyLog(FrappeTestCase):
+	pass
